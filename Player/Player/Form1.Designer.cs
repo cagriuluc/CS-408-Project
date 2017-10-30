4cb8fbf94c9e048f2c735a654e8d87e3b7f1ec33
Cansu <cansuulker96@gmail.com>
2017-10-30 11:03:16 +0200

    Client -updated

--- Player/Player/Form1.Designer.cs
+++ Player/Player/Form1.Designer.cs
@@ -34,9 +34,16 @@
             this.label2 = new System.Windows.Forms.Label();
             this.button_Connect = new System.Windows.Forms.Button();
             this.button_Disconnect = new System.Windows.Forms.Button();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.button_List = new System.Windows.Forms.Button();
+            this.textBox_Message = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button_Send = new System.Windows.Forms.Button();
+            this.textBox_username = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox_IP
@@ -66,7 +73,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(-1, 58);
+            this.label2.Location = new System.Drawing.Point(3, 58);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(39, 13);
             this.label2.TabIndex = 3;
@@ -82,6 +89,7 @@
             this.button_Connect.TabIndex = 4;
             this.button_Connect.Text = "Connect";
             this.button_Connect.UseVisualStyleBackColor = true;
+            this.button_Connect.Click += new System.EventHandler(this.button_Connect_Click);
             // 
             // button_Disconnect
             // 
@@ -93,23 +101,26 @@
             this.button_Disconnect.TabIndex = 5;
             this.button_Disconnect.Text = "Disconnect";
             this.button_Disconnect.UseVisualStyleBackColor = false;
+            this.button_Disconnect.Click += new System.EventHandler(this.button_Disconnect_Click);
             // 
-            // richTextBox1
+            // richTextBox
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(13, 141);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(308, 176);
-            this.richTextBox1.TabIndex = 6;
-            this.richTextBox1.Text = "";
+            this.richTextBox.Location = new System.Drawing.Point(9, 136);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(312, 236);
+            this.richTextBox.TabIndex = 6;
+            this.richTextBox.Text = "";
+            this.richTextBox.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 122);
+            this.label3.Location = new System.Drawing.Point(6, 120);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(49, 13);
             this.label3.TabIndex = 7;
             this.label3.Text = "Activities";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // button_List
             // 
@@ -119,15 +130,84 @@
             this.button_List.TabIndex = 8;
             this.button_List.Text = "List Players";
             this.button_List.UseVisualStyleBackColor = true;
+            this.button_List.Click += new System.EventHandler(this.button_List_Click);
+            // 
+            // textBox_Message
+            // 
+            this.textBox_Message.Location = new System.Drawing.Point(64, 380);
+            this.textBox_Message.Name = "textBox_Message";
+            this.textBox_Message.Size = new System.Drawing.Size(192, 20);
+            this.textBox_Message.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 383);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Message:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // button_Send
+            // 
+            this.button_Send.Location = new System.Drawing.Point(262, 378);
+            this.button_Send.Name = "button_Send";
+            this.button_Send.Size = new System.Drawing.Size(59, 23);
+            this.button_Send.TabIndex = 11;
+            this.button_Send.Text = "Send";
+            this.button_Send.UseVisualStyleBackColor = true;
+            this.button_Send.Click += new System.EventHandler(this.button_Send_Click);
+            // 
+            // textBox_username
+            // 
+            this.textBox_username.Location = new System.Drawing.Point(44, 88);
+            this.textBox_username.Name = "textBox_username";
+            this.textBox_username.Size = new System.Drawing.Size(100, 20);
+            this.textBox_username.TabIndex = 12;
+            this.textBox_username.TextChanged += new System.EventHandler(this.textBox_username_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 91);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Name:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(327, 47);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(140, 353);
+            this.richTextBox1.TabIndex = 14;
+            this.richTextBox1.Text = "";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(327, 31);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "List of Players";
             // 
             // Client
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(347, 341);
+            this.ClientSize = new System.Drawing.Size(487, 413);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox_username);
+            this.Controls.Add(this.button_Send);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox_Message);
             this.Controls.Add(this.button_List);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.richTextBox);
             this.Controls.Add(this.button_Disconnect);
             this.Controls.Add(this.button_Connect);
             this.Controls.Add(this.label2);
@@ -150,9 +230,16 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button_Connect;
         private System.Windows.Forms.Button button_Disconnect;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button_List;
+        private System.Windows.Forms.TextBox textBox_Message;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button_Send;
+        private System.Windows.Forms.TextBox textBox_username;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- Player/Player/Form1.cs
+++ Player/Player/Form1.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*---CLIENT SIDE---
+   Players are able to send messages to other players.
+   They can request a list of players from the server.
+   They can connect/disconnect to the server.
+ */
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +12,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+using System.Net.Sockets;
 
 namespace Player
 {
     public partial class Client : Form
     {
+        bool terminating = false;
+        static Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
         public Client()
         {
             InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -31,5 +42,129 @@ namespace Player
         {
 
         }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Connect_Click(object sender, EventArgs e)
+        {
+            bool connected = false;
+            try
+            {
+                string userName = textBox_username.Text;
+                string serverIP = textBox_IP.Text;
+                int port = Convert.ToInt32(textBox_Port.Text);
+                Thread receiveThread;
+
+
+                clientSocket.Connect(serverIP, port);
+                connected = true;
+                sendUsername(textBox_username.Text);
+                receiveThread = new Thread(new ThreadStart(Receive));
+                receiveThread.Start();
+                richTextBox.AppendText("Connected to the server.");
+            }
+            catch
+            {
+                richTextBox.AppendText("ERROR: Cannot connect to the server.\n");
+                connected = false;
+            }
+        }
+
+
+        void sendMessage(string message)
+        {
+            
+                byte[] buffer = Encoding.Default.GetBytes(message);
+                clientSocket.Send(buffer);
+                richTextBox.AppendText("Message sent.\n");
+            
+
+        }
+        void sendUsername(string name)
+        {
+          
+                byte[] buffer = Encoding.Default.GetBytes(name);
+                clientSocket.Send(buffer);
+            
+        }
+        private void Receive()
+        {
+            bool connected = true;
+         
+            while (connected)
+            {
+                try
+                {
+                    byte[] buffer = new byte[64];
+
+                    int rec = clientSocket.Receive(buffer);
+
+                    if (rec <= 0)
+                    {
+                        throw new SocketException();
+                    }
+                }
+                catch
+                {
+                    if (!terminating)
+                        richTextBox.AppendText("Connection has been terminated.\n");
+                    connected = false;
+                }
+
+
+            }
+
+        }
+
+        private void button_Send_Click(object sender, EventArgs e) //a function to send messages between players
+        {
+           
+            string message = "";
+         
+                message = textBox_Message.Text;
+                try
+                {
+                    sendMessage(message);
+
+                }
+                catch
+                {
+                    terminating = true;
+                    clientSocket.Close();
+                    richTextBox.AppendText("ERROR: Message could not be sent.\n");
+                }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_username_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Disconnect_Click(object sender, EventArgs e)
+        {
+            richTextBox.AppendText("Disconnecting from the server.\n");
+            terminating = true;
+            clientSocket.Dispose();
+            clientSocket.Close();
+        }
+
+        private void button_List_Click(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
